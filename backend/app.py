c6a6b0a60f5d8949e31a9d89b5587857ca3cf622
Elite-pranav <149564675+Elite-pranav@users.noreply.github.com>
2025-02-24 17:02:48 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -80,4 +80,4 @@ def analyze_pestel():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=True)
