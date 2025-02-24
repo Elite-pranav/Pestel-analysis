292004d3ecb76ff53751407df46d87c048a4c1c2
Elite-pranav <149564675+Elite-pranav@users.noreply.github.com>
2025-02-25 01:13:26 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,6 +4,8 @@ const api = axios.create({
   baseURL: "https://pestel-analysis-production.up.railway.app", // Use the deployed backend URL
   headers: {
     "Content-Type": "application/json",
+     timeout: 30000, // Set timeout to 30s (increase if needed)
+
   },
 });
 
